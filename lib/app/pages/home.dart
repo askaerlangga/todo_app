e1eb1c1b9c2445224069e2424850cad10ec22402
askaerlangga <ae.askaerlangga@gmail.com>
2023-09-19 16:52:58 +0700

    update task list

--- lib/app/pages/home.dart
+++ lib/app/pages/home.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_app/app/database/table/task_table.dart';
 import 'package:todo_app/app/provider/task.dart';
 import 'package:todo_app/app/widgets/new_task_button.dart';
 import 'package:todo_app/app/widgets/task_card.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TaskTable _taskTable = TaskTable();
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -17,11 +24,12 @@ class HomePage extends StatelessWidget {
             Consumer<TaskProvider>(builder: (context, value, child) {
           return NewTaskButton(
             onPressed: () {
-              value.addTask(TaskCard(
-                text: 'Test',
-                deleteButtonOnPressed: () {},
-              ));
+              // value.addTask(TaskCard(
+              //   text: 'Test',
+              //   deleteButtonOnPressed: () {},
+              // ));
             },
+            textController: value.newTaskController,
           );
         }),
         appBar: AppBar(
@@ -31,11 +39,26 @@ class HomePage extends StatelessWidget {
             IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
           ],
         ),
-        body: Consumer<TaskProvider>(builder: (context, value, child) {
-          return ListView(
-            children: value.task.toList(),
-          );
-        }),
+        body: FutureBuilder(
+            future: _taskTable.selectAll(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                if (snapshot.data!.isEmpty) {
+                  return const Text('Tidak ada data');
+                }
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return TaskCard(
+                          text: snapshot.data![index].name ?? '',
+                          deleteButtonOnPressed: () {});
+                    });
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }),
       ),
     );
   }
